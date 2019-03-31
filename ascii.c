20b353daf214e741e9a9966898809f60210bdf35
Chris Yang <yljh21328@gmail.com>
2019-03-31 12:41:45 +0800

    Create ascii.c
    practice ascii code

---
+++ ascii.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    char character;
+    printf("Enter a character:");
+    scanf("%c", &character);
+    printf("ASCII value of %c = %d\n", character, character);
+}
